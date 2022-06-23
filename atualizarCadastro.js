a3efd4dd5b76db068a573522e19eb7cf6a60ab35
Jean "Polako" Monteiro <84013209+JeanMonteiroPLK@users.noreply.github.com>
2022-06-23 17:30:46 -0300

    Funcional (sem arduino) - NÃO alterar!

--- atualizarCadastro.js
+++ atualizarCadastro.js
@@ -1,6 +1,8 @@
 var listaDeUsuarios = []
-var usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"))
+var listaDeResponsaveis = []
+var usuarioLogado
 var posicao = -1
+var posicaoResponsavel = -1
 var novoValorInput = document.getElementById('novoValorInput')
 
 var selectAtributosUsuario = document.querySelector('#atributosUsuario')
@@ -8,15 +10,16 @@ selectAtributosUsuario.addEventListener('change', function () { })
 
 function editarUsuario() {
     listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
+    listaDeResponsaveis = JSON.parse(localStorage.getItem("responsaveisKey"))
+    
+    if(document.getElementById("atributosResp")!= null)
+    selectAtributosUsuario = document.getElementById("atributosResp")
     if (ehSelectValido()) {
         if (!ehInputVazio(novoValorInput.value)) {
             switch (selectAtributosUsuario.value) {
 
                 case 'nome':
                     listaDeUsuarios[posicao].nome = novoValorInput.value
-                    if (usuarioLogado.nome == listaDeUsuarios[posicao].nome) {
-                        usuarioLogado.nome = novoValorInput.value
-                    }
                     break
 
                 case 'cpf':
@@ -45,10 +48,43 @@ function editarUsuario() {
 
                 case 'senha':
                     listaDeUsuarios[posicao].senha = novoValorInput.value
+                    if(posicaoResponsavel != -1){
+                        listaDeResponsaveis[posicaoResponsavel].senhaResp = novoValorInput.value
+                    }
                     break
+
+                    case 'nomeResponsavel':
+                        listaDeResponsaveis[posicaoResponsavel].nomeResp = novoValorInput.value
+                        break
+                        
+    
+                    case 'cpfResponsavel':
+                        listaDeResponsaveis[posicaoResponsavel].cpfResp = novoValorInput.value
+                        break
+    
+                    case 'cidadeResponsavel':
+                        listaDeResponsaveis[posicaoResponsavel].enderecoResp.cidadeResp = novoValorInput.value
+                        break
+    
+                    case 'bairroResponsavel':
+                        listaDeResponsaveis[posicaoResponsavel].enderecoResp.bairroResp = novoValorInput.value
+                        break
+    
+                    case 'ruaResponsavel':
+                        listaDeResponsaveis[posicaoResponsavel].enderecoResp.ruaResp = novoValorInput.value
+                        break
+    
+                    case 'numeroResponsavel':
+                        listaDeResponsaveis[posicaoResponsavel].enderecoResp.numeroResp = novoValorInput.value
+                        break
+    
+                    case 'complementoResponsavel':
+                        listaDeResponsaveis[posicaoResponsavel].enderecoResp.complementoResp = novoValorInput.value
+                        break
             }
 
             localStorage.setItem("usuariosKey", JSON.stringify(listaDeUsuarios))
+            localStorage.setItem("responsaveisKey", JSON.stringify(listaDeResponsaveis))
             localStorage.setItem("usuarioLogado", JSON.stringify(usuarioLogado))
             alert(`Usuário editado!`)
 
@@ -63,20 +99,21 @@ function editarUsuario() {
     }
 }
 
-function acharLogadoNaListaDeUsuarios() {
+function encontrarLogadoNaListaDeUsuarios() {
     listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
+    usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"))
     var flag = 0
-
+    
     if (usuarioLogado == '' || usuarioLogado == null || usuarioLogado == undefined) {
         alert('ERRO: Não há usuário logado!')
         window.location.href = "inicioOUT.html"
         return
     }
-
+    
     for (i = 0; i < listaDeUsuarios.length; i++) {
         if (usuarioLogado.id == listaDeUsuarios[i].id) {
             flag = 1
-            posicao = i
+            this.posicao = i
         }
     }
 
@@ -84,6 +121,75 @@ function acharLogadoNaListaDeUsuarios() {
         alert('Atenção [ADMIN]! Inicie sua própria sessão para editar cadastros!')
         window.location.href = "inicioOUT.html"
     }
+}
+
+function deletarUsuarioLogado() {
+    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
+    listaDeResponsaveis = JSON.parse(localStorage.getItem("responsaveisKey"))
+    usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"))
+
+    encontrarLogadoNaListaDeUsuarios()
+    listaDeUsuarios.splice(this.posicao, 1)
+    localStorage.setItem("usuariosKey", JSON.stringify(listaDeUsuarios))
+
+    if(temResponsavel()){
+        deletarResponsavel()
+    }
+
+    alert('Seu cadastro foi excluído!')
+    limparObjetoLogado()
+    window.location.href = 'inicioOUT.html'
+}
+
+function deletarResponsavel() {
+    listaDeResponsaveis = JSON.parse(localStorage.getItem("responsaveisKey"))
+    usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"))
+    if (temResponsavel()) {
+        listaDeResponsaveis.splice(this.posicaoResponsavel, 1)
+        localStorage.setItem("responsaveisKey", JSON.stringify(listaDeResponsaveis))
+        alert('Responsável excluído!')
+    } else {
+        alert('Você não possui responsável')
+    }
+}
+
+function temResponsavel(){
+    var retorno = false
+    listaDeResponsaveis = JSON.parse(localStorage.getItem("responsaveisKey"))
+    usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"))
+    for(i = 0; i < listaDeResponsaveis.length; i++){
+        if(listaDeResponsaveis[i].idResp == usuarioLogado.id){
+            this.posicaoResponsavel = i
+            selectResponsavel()
+            retorno = true
+        }
+    }
+    return retorno
+}
+
+function selectResponsavel(){
+    document.getElementById("atributosUsuario").innerHTML = `<select name="atributosResp" id="atributosResp">\n
+    <option value="" disabled selected>Selecione...</option>\n
+    <option value="nome">NOME</option>\n
+    <option value="cpf">CPF</option>\n
+    <option value="cidade">CIDADE</option>\n
+    <option value="bairro">BAIRRO</option>\n
+    <option value="rua">RUA</option>\n
+    <option value="numero">NÚMERO</option>\n
+    <option value="complemento">COMPLEMENTO</option>\n
+    <option value="senha">SENHA</option>\n
+   <option value="nomeResponsavel">NOME DO RESPONSAVEL</option>\n
+   <option value="cpfResponsavel">CPF DO RESPONSAVEL</option>\n
+   <option value="cidadeResponsavel">CIDADE DO RESPONSAVEL</option>\n
+   <option value="bairroResponsavel">BAIRRO DO RESPONSAVEL</option>\n
+   <option value="ruaResponsavel">RUA DO RESPONSAVEL</option>\n
+   <option value="numeroResponsavel">NUMERO DO RESPONSAVEL</option>\n
+   <option value="complementoResponsavel">COMPLEMENTO DO RESPONSAVEL</option> </select> <br>`
+
+}
+
+function limparObjetoLogado() {
+    localStorage.setItem('usuarioLogado', '')
 }
 
 function ehSelectValido() {
